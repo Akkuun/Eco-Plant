8f4b326933c5010b23485a9e62f20138e0527395
unknown <mathisduban@gmail.com>
2025-04-16 12:04:45 +0200

    add all fonts

--- app/src/main/java/com/example/eco_plant/ui/theme/Typography.kt
+++ app/src/main/java/com/example/eco_plant/ui/theme/Typography.kt
@@ -7,26 +7,112 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.eco_plant.R
 
-// Define custom font family
+// Define the Inter font family
 val InterFontFamily = FontFamily(
-    Font(R.font.inter_24pt_extrabold, FontWeight.ExtraBold)
+    Font(R.font.inter_24pt_extrabold, FontWeight.ExtraBold), //ExtraBold
+    Font(R.font.inter_18pt_semibold, FontWeight.SemiBold), //SemiBold
+    Font(R.font.inter_18pt_bold, FontWeight.Bold), //Bold
+    Font(R.font.inter_18pt_regular, FontWeight.Normal) //Regular
 )
 
-// Define typography styles
-val InterTypography = androidx.compose.material3.Typography(
-    displayLarge = TextStyle(
+// Define typography categories
+// Headings category -> done
+object Heading {
+    val H1 = TextStyle(
         fontFamily = InterFontFamily,
         fontSize = 24.sp,
         fontWeight = FontWeight.ExtraBold
-    ),
-    headlineMedium = TextStyle(
+    )
+    val H2 = TextStyle(
         fontFamily = InterFontFamily,
         fontSize = 18.sp,
         fontWeight = FontWeight.ExtraBold
-    ),
-    titleSmall = TextStyle(
+    )
+    val H3 = TextStyle(
         fontFamily = InterFontFamily,
-        fontSize = 14.sp,
+        fontSize = 16.sp,
         fontWeight = FontWeight.ExtraBold
     )
+    val H4 = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold
+    )
+    val H5 = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold
+    )
+
+}
+
+// Body category -> done
+object Body {
+    val XL = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val L = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val M = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val S = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Normal
+    )
+    val XS = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.Medium
+    )
+
+}
+
+// Action category -> done
+object Action {
+    val L = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+    val M = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+    val S = TextStyle(
+        fontFamily = InterFontFamily,
+        fontSize = 10.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+}
+
+
+// Combine all styles into a single Typography object
+val InterTypography = androidx.compose.material3.Typography(
+    //Headings
+    displayLarge = Heading.H1,
+    displayMedium = Heading.H2,
+    displaySmall = Heading.H3,
+    headlineLarge = Heading.H4,
+    headlineMedium = Heading.H5,
+    //Body
+    bodyLarge = Body.XL,
+    bodyMedium = Body.L,
+    bodySmall = Body.M,
+    //Action
+    labelLarge = Action.L,
+    labelMedium = Action.M,
+    labelSmall = Action.S,
+
+
+
 )
